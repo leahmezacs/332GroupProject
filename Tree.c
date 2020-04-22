158e6290b5badeec722aef20219711df70783200
Afsana Akter <aakter002@citymail.cuny.edu>
2020-04-21 21:31:11 -0400

    Create Tree.c

---
+++ Tree.c
@@ -0,0 +1,48 @@
+//Afsana Akter
+//for the Tree portion
+
+#include<stdio.h> 
+#include<sys/types.h> 
+#include<sys/stat.h> 
+#include<unistd.h> 
+#include<fcntl.h> 
+#include<stdlib.h> 
+
+int main()
+{ 
+     // creating directory dir0 
+     struct stat st = {0}; 
+     if(stat("./Dir0", &st) == -1)
+     { 
+          mkdir("./Dir0", 0700); 
+     } 
+
+    // changing directory to Dir0 
+    chdir("Dir0"); 
+
+    // Creating files inside Dir0 
+    int fileDescriptor = open("t1.txt", O_RDWR | O_APPEND | O_CREAT); 
+    if(fileDescriptor < 0)
+    { 
+           perror("Error creating file"); 
+           exit(-1); 
+    } 
+    fileDescriptor = open("t2.txt", O_RDWR | O_APPEND | O_CREAT); 
+    if(fileDescriptor < 0)
+    { 
+           perror("Error creating file"); 
+           exit(-1); 
+    } 
+    fileDescriptor = open("t3.txt", O_RDWR | O_APPEND | O_CREAT); 
+    if(fileDescriptor < 0)
+    { 
+           perror("Error creating file"); 
+           exit(-1); 
+     } 
+     // creating directory dir1 
+     if(stat("./Dir1", &st) == -1)
+     { 
+           mkdir("./Dir1", 0700); 
+     } 
+    
+} 
